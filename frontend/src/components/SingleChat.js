1fff7ae27fc8612f60c461b07c813edd15d58cac
rachana-uniyal <rachanauniyal0417@gmail.com>
2022-06-29 16:33:05 +0530

    added updated endpoint

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -15,7 +15,7 @@ import animationData from "../animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT =  "https://mearn-talk-and-talk.herokuapp.com/"
+const ENDPOINT =  "https://talk-and-talk.herokuapp.com/"
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setfetchAgain }) => {
